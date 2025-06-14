fc2d1380cd2125db0c6befffb3b9452cb007b595
JustJoaoSantos <justa.redfox2002@gmail.com>
2025-06-14 14:51:13 -0300

    concluido desafio de poo com abstracao de Celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("(71)92244-4422", "Galaxy 2", "2222222", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\nSmartphone Iphone");
+Iphone iphone = new Iphone("(71)91122-2211", "Iphone 4", "3333333", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
